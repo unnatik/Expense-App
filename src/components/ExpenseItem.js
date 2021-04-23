c75daa069f1c6f25adde7f3e2acd53663e86fa58
Unnati <unnatikolhe@gmail.com>
2021-04-23 17:18:01 +0530

    add ExpenseItem

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function ExpenseItem(props) {
+    return (
+        <div>
+            <h2>Expense Item</h2>
+        </div>
+    )
+}
+
+ExpenseItem.propTypes = {
+
+}
+
+export default ExpenseItem
+
+
+
